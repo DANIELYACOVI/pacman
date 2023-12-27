0f9b35768b21894638b54aed90e660794fcc5fc7
DANIELYACOVI <145760623+DANIELYACOVI@users.noreply.github.com>
2023-12-27 13:17:09 +0200

    initial Upload

---
+++ js/game.js
@@ -0,0 +1,150 @@
+'use strict'
+
+const WALL = '&#8251;'
+const FOOD = '&middot;'
+const EMPTY = ' '
+const SUPERFOOD = '🍟'
+const CHERRY = '🍒'
+
+const SUPERFOOD_GHOST_COLOR = 'BLUE'
+
+var gSuperPowerMode = false
+var gSuperPowerTimer
+var gCherryInterval
+
+
+const gGame = {
+    score: 0,
+    isOn: false,
+    isVictory: false,
+    foodCount: 0
+}
+var gBoard
+
+function init() {
+
+    gBoard = buildBoard()
+    createPacman(gBoard)
+    createGhosts(gBoard)
+    
+    renderBoard(gBoard, '.board-container')
+    gGame.isOn = true
+
+    gGame.foodCount = countTotalFood(gBoard)
+    gCherryInterval = setInterval(addCherry, 15000)
+
+    gSuperPowerMode = false
+}
+
+function countTotalFood(board) {
+    var count = 0
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j++) {
+            if (board[i][j] === FOOD) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+function buildBoard() {
+    const size = 10
+    const board = []
+
+    for (var i = 0; i < size; i++) {
+        board.push([]) // board[i] = []
+
+        for (var j = 0; j < size; j++) {
+            board[i][j] = FOOD
+            gGame.foodCount++
+            
+            if (i === 0 || i === size - 1 ||
+                j === 0 || j === size - 1 ||
+                (j === 3 && i > 4 && i < size - 2)) {
+                board[i][j] = WALL
+                gGame.foodCount--
+            }
+
+            if(i === 1 && j === 1 || i === 1 && j === size - 2 ||
+                j === 1 && i === size - 2 || j === size - 2 && i === size - 2){
+                 board[i][j] = SUPERFOOD
+                 gGame.foodCount -= 4
+                }
+        }
+    }
+    return board
+}
+
+function updateScore(diff) {
+    const elScore = document.querySelector('h2 span')
+
+    // Model
+    gGame.score += diff
+    // DOM
+    elScore.innerText = gGame.score
+}
+
+
+function addCherry(){
+    const emptyLocation = getEmptyLocation(gBoard)
+    if(!emptyLocation) return
+    gBoard[emptyLocation.i][emptyLocation.j] = CHERRY
+    renderCell(emptyLocation, CHERRY)
+}
+
+function getEmptyLocation(board){
+    const emptyLocations = []
+    for (var i = 0; i < board.length; i++){
+        for(var j = 0; j <board[0].length; j++){
+            if(board[i][j] === EMPTY){
+                emptyLocations.push({ i, j})
+            }
+        }
+    }
+    return emptyLocations.length > 0 ? emptyLocations[Math.floor(Math.random() * emptyLocations.length)] : null
+}
+function gameOver() {
+    console.log('Game Over')
+    if (!gGame.isOn) return
+    clearInterval(gGhostsInterval)
+    clearInterval(gCherryInterval)
+    gGame.isOn = false
+    displayGameOverModal()
+    checkVictory()
+    gGhosts = []
+}
+
+
+function displayGameOverModal() {
+    const modalOverlay = document.getElementById('modalOverlay');
+    const gameOverModal = document.getElementById('gameOverModal');
+    
+    const scoreSpan = gameOverModal.querySelector('.score-span');
+    scoreSpan.textContent = `Your Score: ${gGame.score}`;
+
+    modalOverlay.style.display = 'block';
+    gameOverModal.style.display = 'block';
+}
+
+function playAgain(){
+    const modalOverlay = document.getElementById('modalOverlay')
+    const gameOverModal = document.getElementById('gameOverModal')
+    
+    gGame.isOn = true
+    gGame.score = 0
+    updateScore(0)
+    
+    modalOverlay.style.display = 'none'
+    gameOverModal.style.display = 'none'
+    init()
+}
+
+function checkVictory() {
+    console.log(gGame.foodCount);
+    if (gGame.foodCount === 0) {
+        gGame.isVictory = true
+        gameOver()
+        console.log('finish');
+    }
+}
---
+++ js/pacman.js
@@ -0,0 +1,156 @@
+'use strict'
+
+const PACMAN = '🙃'
+var gPacman
+
+function createPacman(board) {
+    // TODO: initialize gPacman...
+    gPacman = {
+        location: { i: 3, j: 5 },
+        isSuper: false,
+        deg: 0
+    }
+    board[gPacman.location.i][gPacman.location.j] = PACMAN
+    gGame.foodCount--
+}
+
+
+function movePacman(ev) {
+
+    if (!gGame.isOn) return
+
+    // TODO: use getNextLocation(), nextCell
+    const nextLocation = getNextLocation(ev)
+    if (!nextLocation) return
+
+    const nextCell = gBoard[nextLocation.i][nextLocation.j]
+
+    // TODO: return if cannot move
+    if (nextCell === WALL) return
+
+    // TODO: hitting a ghost? call gameOver
+    if (nextCell === GHOST) {
+        if (gPacman.isSuper) {
+            eatGhost(nextLocation)
+        } else {
+            gameOver()
+            return
+        }
+    }
+    // TODO: hitting food? call updateScore
+    if (nextCell === FOOD) {
+        updateScore(1)
+        gGame.foodCount--
+        checkVictory()
+        gBoard[nextLocation.i][nextLocation.j] = EMPTY
+    }
+
+    if (nextCell === SUPERFOOD) {
+        if (gPacman.isSuper) return
+        eatSuperFood()
+    }
+
+    if (nextCell === CHERRY) {
+        updateScore(10)
+    }
+
+    // TODO: moving from current location:
+    // TODO: update the model
+    gBoard[gPacman.location.i][gPacman.location.j] = EMPTY
+
+    // TODO: update the DOM
+    renderCell(gPacman.location, EMPTY)
+
+    // TODO: Move the pacman to new location:
+    // TODO: update the model
+    gPacman.location = nextLocation
+    gBoard[gPacman.location.i][gPacman.location.j] = PACMAN
+
+    // TODO: update the DOM
+    renderCell(gPacman.location, PACMAN)
+    
+    if (nextCell === SUPERFOOD && !gSuperPowerMode) {
+        gSuperPowerMode = true;
+        
+        for (var i = 0; i < gGhosts.length; i++) {
+            gGhosts[i].color = SUPERFOOD_GHOST_COLOR
+        }
+        
+        gSuperPowerTimer = setTimeout(() => {
+            endSuperPowerMode();
+        }, 5000)
+    } else if (nextCell === SUPERFOOD && gSuperPowerMode) {
+        return
+    } else if (nextCell === GHOST) {
+        eatGhost(nextLocation);
+    } else if (nextCell === WALL) {
+        return
+    }
+}
+
+function endSuperPowerMode() {
+    gSuperPowerMode = false
+
+    gGhosts.forEach((ghost) => {
+        ghost.color = getRandomColor()
+    })
+
+    reviveEatenGhosts()
+
+    clearTimeout(gSuperPowerTimer)
+}
+
+function getNextLocation(eventKeyboard) {
+    const nextLocation = { i: gPacman.location.i, j: gPacman.location.j }
+
+    switch (eventKeyboard.key) {
+        case 'ArrowUp':
+            nextLocation.i--
+            break;
+
+        case 'ArrowDown':
+            nextLocation.i++
+            break;
+
+        case 'ArrowLeft':
+            nextLocation.j--
+            break;
+
+        case 'ArrowRight':
+            nextLocation.j++
+            break;
+
+        default: return null
+    }
+    return nextLocation
+}
+
+function eatGhost(location) {
+    for (var i = 0; i < gGhosts.length; i++) {
+        const currLocation = gGhosts[i].location
+        if (currLocation.i === location.i && currLocation.j === location.j) {
+            const deadGhost = gGhosts.splice(i, 1)[0]
+            checkGhostCellContent(deadGhost)
+            gDeadGhosts.push(deadGhost)
+        }
+    }
+}
+
+function checkGhostCellContent(ghost){
+    if(ghost.currCellContent === FOOD){
+        updateScore(10)
+        ghost.currCellContent = EMPTY
+    }
+}
+
+function eatSuperFood() {
+    gPacman.isSuper = true
+    renderGhosts()
+    setTimeout(() => {
+        gPacman.isSuper = false
+        reviveEatenGhosts()
+        renderGhosts()
+        checkVictory()
+    }, 5000)
+}
+
